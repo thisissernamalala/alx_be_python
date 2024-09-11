4a83a8de920c5b58d98bb89b8255df2c06ace5ce
thisissernamalala <222650494@mycput.ac.za>
2024-09-11 16:58:17 +0200

    Basic math operations python code

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#Adding two numbers
+#subtracting two numbers
+#multiplying two numbers
+
+#basic math operations
+
+number1 = 10
+number2 = 5
+
+add = number1+number2
+minus = number1-number2
+mult = number1*number2
+
+print(f"Addition of {number1} and {number2} is {add}")
+print(f"Subtraction of {number1} and {number2} is {minus}")
+print(f"Multiplication of {number1} and {number2} is {mult}")
