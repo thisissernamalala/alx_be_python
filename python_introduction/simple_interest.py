728d1037a0f15bf5b70e95534d245097be6e6d3c
thisissernamalala <222650494@mycput.ac.za>
2024-09-11 17:02:24 +0200

    simple interest code

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#code to calculate simple interest using principle amount, the rate and time period
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal*rate*time
+
+print(f"The simple interest is: {interest}")
