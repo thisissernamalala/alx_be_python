43f3753b7e416b156d6c2662d215e7d90286ee8c
thisissernamalala <222650494@mycput.ac.za>
2024-09-11 17:16:33 +0200

    Code to calculate a future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+#a calculator that calculates a persons age in a future year eg. 2077
+
+age = int(input("How old are you?"))
+future_age = age + 27
+
+print(f"In 2050, you will be {future_age} years old.")
