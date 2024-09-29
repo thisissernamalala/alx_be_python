bb6e5cb43c90f2baf01713a8539cc30a36a33b68
thisissernamalala <222650494@mycput.ac.za>
2024-09-29 20:57:55 +0200

    revised the code to use the datetime module according to the exercise given

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    print(f"Current date and time: {current_date}")
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_date}")
 
 def calculate_future_date():
     current_date = datetime.now()
