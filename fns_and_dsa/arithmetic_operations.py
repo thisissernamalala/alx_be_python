79c167374db96721e0a7ea4c2039ceabc4610527
thisissernamalala <222650494@mycput.ac.za>
2024-09-29 20:19:22 +0200

    Made a calculate function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,8 +6,9 @@ def perform_operation(num1, num2, operation):
         result = num1-num2
     elif operation == "multiply":
         result = num1*num2
-    elif operation == "divide" and num2 != 0:
-        result = num1/num2
-    else:
-        result = "Cannot divide by zero"
+    elif operation == "divide":
+        if num2 == 0:
+            result = "Cannot divide by zero"
+        else:
+            result = num1/num2
     return result
