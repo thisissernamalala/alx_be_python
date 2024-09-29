b29d3863dd4c274b86255c67f67dff1ef5e640d5
thisissernamalala <222650494@mycput.ac.za>
2024-09-29 21:29:03 +0200

    trying to fix the problems stated by the autochecker

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,11 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - 32.0) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32.0
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 try:
     temp = int(input("Enter the temperature to convert: "))
