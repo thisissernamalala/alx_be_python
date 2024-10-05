f198e0afd581fb17d1ecd378f60e543a3ea34330
thisissernamalala <222650494@mycput.ac.za>
2024-10-05 20:53:29 +0200

    made changes to safe division progarm, used exception handlin

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,10 +3,10 @@ def safe_divide(numerator, denominator):
         numerator = float(numerator)
         denominator = float(denominator)
         result = numerator/denominator
-        print(f"The result of the division is {result:.2f}")
+        return (f"The result of the division is {result:.2f}")
 
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        return ("Error: Cannot divide by zero.")
     
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return ("Error: Please enter numeric values only.")
