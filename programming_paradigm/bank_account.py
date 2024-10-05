50650ab273bab2a3010a8fc07ebd456d27d0420a
thisissernamalala <222650494@mycput.ac.za>
2024-10-05 17:23:45 +0200

    created bank account class with functions

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,5 +14,5 @@ class BankAccount:
             return False
             
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:.2f}.")
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
